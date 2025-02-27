53bfed0e73de012f1cb3181622ed15a0b20df303
Az <arbusinos@up.edu.ph>
2025-02-28 02:53:47 +0800

    [fix][az] fixes google log in partially

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './assets/main.css'
 import { OhVueIcon, addIcons } from 'oh-vue-icons'
 import { BiCaretDownFill, FcGoogle, BiCalendar } from 'oh-vue-icons/icons'
-import { createGoogleAuth } from 'vue3-google-login'
+import vue3GoogleLogin from 'vue3-google-login'
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -11,11 +11,9 @@ addIcons(BiCaretDownFill, FcGoogle, BiCalendar)
 
 const app = createApp(App)
 
-app.use(
-  createGoogleAuth({
-    clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
-  }),
-)
+app.use(vue3GoogleLogin, {
+  clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+})
 
 app.use(router)
 app.component('v-icon', OhVueIcon)
