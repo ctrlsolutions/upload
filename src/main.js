d83521db36554c01a68e6a717a51fcbfa1ef5aba
Az <arbusinos@up.edu.ph>
2025-02-28 01:33:16 +0800

    [feat][az] adds google log in dependencies

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './assets/main.css'
 import { OhVueIcon, addIcons } from 'oh-vue-icons'
-import { BiCaretDownFill, FcGoogle, BiCalendar} from 'oh-vue-icons/icons'
-
+import { BiCaretDownFill, FcGoogle, BiCalendar } from 'oh-vue-icons/icons'
+import { createGoogleAuth } from 'vue3-google-login'
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -9,8 +9,13 @@ import router from './router'
 
 addIcons(BiCaretDownFill, FcGoogle, BiCalendar)
 
-
 const app = createApp(App)
+
+app.use(
+  createGoogleAuth({
+    clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+  }),
+)
 
 app.use(router)
 app.component('v-icon', OhVueIcon)
