a6b2565681f3339c58712f3970d06f2a5d1a91cb
Az <arbusinos@up.edu.ph>
2025-03-13 03:11:20 +0800

    [refactor][az] refactors landing page to use a layout

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -1,67 +1,24 @@
 <template>
-  <div class="static-page">
-    <div class="scroll-container">
-      <section class="page"><Home></Home></section>
-      <section class="page"><About></About></section>
-      <section class="page"><Summary></Summary></section>
-    </div>
-
-    <div class="background">
-      <img src="@/assets/Oble.png" class="bg-image" />
-    </div>
-  </div>
+  <LandingLayout>
+    <section class="page"><Home /></section>
+    <section class="page"><About /></section>
+    <section class="page"><Summary /></section>
+  </LandingLayout>
 </template>
 
-<script>
+<script setup lang="ts">
+import LandingLayout from '@/layouts/LandingPageLayout.vue'
 import Home from '@/components/Landing/Home.vue'
 import About from '@/components/Landing/About.vue'
 import Summary from '@/components/Landing/Summary.vue'
-
-export default {
-  components: {
-    Home,
-    About,
-    Summary,
-  },
-}
 </script>
 
-<style>
-.static-page {
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  flex-direction: row;
-}
-.scroll-container {
-  display: flex;
-  flex-direction: column;
-  scroll-snap-type: y mandatory;
-  scroll-snap-points-y: repeat(300px);
-  overflow-y: scroll;
-  width: 50vw;
-  scroll-behavior: smooth;
-  scrollbar-width: none;
-}
+<style lang="scss" scoped>
 .page {
-  background-color: white;
   scroll-snap-align: start;
-  background-color: transparent;
+  min-height: 100vh;
   display: flex;
-  height: 100vh;
-}
-.background {
-  z-index: 1;
-  display: flex;
-  justify-content: right;
   align-items: center;
-  width: 50vw;
-  height: 100vh;
-  pointer-events: auto;
-}
-.bg-image {
-  width: auto;
-  height: 100vh;
-  object-fit: contain;
+  justify-content: left;
 }
 </style>
