6bc49e88d5b58310436f201e45198d40fd5fec72
Az <arbusinos@up.edu.ph>
2025-03-14 04:27:55 +0800

    [feat:partial][az] adds structure of template of profile page

---
+++ src/pages/ProfilePage.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <div class="upper-part">
+      <!-- USE CARD WITH HEADER -->
+      <div>
+        <img src="" alt="" />
+        <div class="profile-container">
+          <h4 class="name"></h4>
+          <h6 class="role"></h6>
+          <p class="college"></p>
+        </div>
+      </div>
+      <!-- \ USE CARD WITH HEADER -->
+      <!-- GREEN PART -->
+      <div class="green-container">
+        <div class="num-reports"></div>
+        <div class="desc-container">
+          <p class="desc"></p>
+          <p class="year"></p>
+        </div>
+      </div>
+    </div>
+    <div class="lower-part">
+      <div>
+        <div class="info-container" id="left-info">
+          <p class="info-type"></p>
+          <p class="info"></p>
+          <p class="info-type"></p>
+          <p class="info"></p>
+          <p class="info-type"></p>
+          <p class="info"></p>
+        </div>
+        <div class="info-container" id="right-info">
+          <p class="info-type"></p>
+          <p class="info"></p>
+          <p class="info-type"></p>
+          <p class="info"></p>
+          <p class="info-type"></p>
+          <p class="info"></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup></script>
+<style lang="scss" scoped></style>
