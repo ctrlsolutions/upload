dad75d5dcba65f96cd4297ae2e17783357021ac3
ndb3llo <ndbello@up.edu.ph>
2025-03-17 11:20:29 +0800

    [feat][Nico] Adds all the Dashboard Components

---
+++ src/components/Dashboard/MyCalendar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="my-calendar">
+    <p>Coming Soon</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyCalendar",
+};
+</script>
+
+<style scoped>
+.my-calendar {
+  width: 593px;
+  height: 604px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: linear-gradient(to right, #1a3d21, #2f6035);
+  border: 2px solid #ccc;
+  border-radius: 8px;
+  color: #888;
+  font-size: 1.2rem;
+  font-weight: bold;
+}
+</style>
