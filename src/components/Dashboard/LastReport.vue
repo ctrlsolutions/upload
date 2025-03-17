851ddd342306fe1019e088aab316c5262a0a89f9
ndb3llo <ndbello@up.edu.ph>
2025-03-17 11:20:29 +0800

    [feat][Nico] Adds all the Dashboard Components

---
+++ src/components/Dashboard/LastReport.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="last-report">
+    <p>Coming Soon</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LastReport",
+};
+</script>
+
+<style scoped>
+.last-report {
+  width: 656px;
+  height: 322px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: linear-gradient(to right, #1a3d21, #2f6035);
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  color: #fff;
+  font-size: 18px;
+  font-weight: bold;
+}
+</style>
