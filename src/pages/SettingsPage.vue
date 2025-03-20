1e5f35d3981f7c29c69a9c79ea7bb29ecf7672f6
Az <arbusinos@up.edu.ph>
2025-03-20 18:00:23 +0800

    [feat][az] adds settings page template

---
+++ src/pages/SettingsPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <!-- USE CARD WITH HEADER: WHOLE CONTAINER -->
+  <div>
+    <!-- USE NORMAL CARD: USER INFO -->
+    <div>
+      <img src="" alt="" />
+      <div class="user-info">
+        <p class="name"></p>
+        <p class="role"></p>
+        <p class="college"></p>
+      </div>
+      <div class="button-container">
+        <BaseFormButton></BaseFormButton>
+        <BaseFormButton></BaseFormButton>
+      </div>
+    </div>
+    <!-- USE FOLDER COMPONENT -->
+    <div>
+      <div class="first-row">
+        <BaseTextInput></BaseTextInput>
+        <BaseTextInput></BaseTextInput>
+        <BaseTextInput></BaseTextInput>
+      </div>
+      <div class="second-row">
+        <BaseTextInput></BaseTextInput>
+        <BaseTextInput></BaseTextInput>
+        <BaseTextInput></BaseTextInput>
+      </div>
+      <div class="button-placement">
+        <BaseFormButton></BaseFormButton>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import BaseFormButton from '@/components/Global/BaseFormButton.vue'
+import BaseTextInput from '@/components/Global/BaseTextInput.vue'
+</script>
+<style lang="scss" scoped></style>
