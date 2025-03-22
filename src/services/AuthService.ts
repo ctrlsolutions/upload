bef28c45ba00bdb61db79a124978ad6b9346c3bf
Lanieza <lblopez@up.edu.ph>
2025-03-22 18:26:10 +0800

    [feat][lanie] resolve merge conflicts w/ develop and home branch

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -30,7 +30,6 @@ export const googleLogin = async (accessToken: string): Promise<void> => {
   await axios.post(`${API_BASE_URL}/user/google/signup/`, {
     access_token: accessToken,
   })
-<<<<<<< HEAD
 }
 
 export const signupUser = async (
@@ -68,6 +67,3 @@ export const googleSignup = async (
     return { success: false, error: errorMessage }
   }
 }
-=======
-}
->>>>>>> d379585 ([feat][lanie] create dashboard service and modify auth service.)
