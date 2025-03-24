4872e90d6e0d3fb815db277ca4a1e7de5ad0834f
Erl <136324144+ermontano@users.noreply.github.com>
2025-03-24 23:45:32 +0800

    [Feat][Erl] Refactored Profile page

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="upper-part">
-      <div class="profile-card">
+      <CardComponent class="profile-card" width="46.5rem" height="23rem">
         <div class="header">My Profile</div>
         <div class="content">
           <img 
@@ -15,16 +15,16 @@
             <p class="college">College of Science</p>
           </div>
         </div>
-      </div>
-      <div class="green-container">
+      </CardComponent>
+      <CardComponent class="green-container" width="46.5rem" height="23rem">
         <div class="num-reports">1269</div>
         <div class="desc-container">
           <p class="desc">Record of Report Submissions</p>
           <p class="year">2024 - 2025</p>
         </div>
-      </div>
+      </CardComponent>
     </div>
-    <div class="lower-part">
+    <CardComponent class="lower-part" width="94rem" height="28rem">
       <div class="header">Personal Information</div>
       <div class="content">
         <div class="info-grid">
@@ -58,30 +58,39 @@
           </div>
         </div>
       </div>
-    </div>
+    </CardComponent>
   </div>
 </template>
 
 <script lang="ts" setup>
-// Component logic can be added here
+import CardComponent from '@/components/Global/CardComponent.vue';
 </script>
 
 <style lang="scss" scoped>
 .container {
   width: 100%;
   margin: 0;
-  padding: 2rem 2rem 2rem 1rem;
+  padding: 0rem 0rem 4rem 2rem;
   display: grid;
-  gap: 1.5rem;
+  grid-template-columns: 2fr 2fr;
+  grid-template-rows: auto auto; 
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
 .header {
   background: #006837;
   color: white;
   padding: 1rem 2rem;
-  border-radius: 18px;
   font-weight: 500;
   font-size: 1.1rem;
+  width: 100%;
+  margin: -1rem -1rem 0;
+  width: calc(100% + 2rem);
 }
 
 .content {
@@ -105,7 +114,6 @@
     display: flex;
     align-items: flex-start;
     gap: 1.5rem;
-
   }
 
   .profile-image {
@@ -189,17 +197,22 @@
   background: white;
   border-radius: 20px;
   overflow: hidden;
-  width: 100%;  
+  width: 100%;
+  grid-column: span 2;
 }
 
 .info-grid {
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 3rem;
-  padding-left: 4rem;
+  padding-left: 4rem 4rem;
 }
 
 .info-container {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  padding-left: 2rem;
   .info-group {
     margin-bottom: 1.5rem;
 
@@ -219,6 +232,7 @@
     font-size: 1.1rem;
     font-weight: 500;
     margin: 0;
+    padding-left: 1rem;
   }
 }
 </style>
