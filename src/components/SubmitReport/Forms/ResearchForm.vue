30eef344a6ee82e7419d3d754fdb443184a5018f
Randall Alquicer <randall.bposeats@gmail.com>
2025-04-05 21:12:22 +0800

    abstracts info div from submit report page

--- src/components/SubmitReport/Forms/ResearchForm.vue
+++ src/components/SubmitReport/Forms/ResearchForm.vue
@@ -1,13 +1,7 @@
 <template>
-    <div class="info-container" v-if="infoVisible">
-        <v-icon name="bi-book" scale="2" />
-        <div class="info-text">
-            hahahahahahas hsiachihac hiahsichaischas
-            hahahahahahas hsiachihac hiahsichaischas
-            hahahahahahas hsiachihac hiahsichaischas
-            
-        </div>      
-    </div>
+    <InformationDiv :infoVisible="infoVisible" >
+        hahahah
+    </InformationDiv>
     <div class="form-container">
         <div class="form">
             <div class="form-group">
@@ -42,6 +36,7 @@
 <script setup>
 import { ref } from 'vue'
 import BaseDateInput from '@/components/Global/BaseDateInput.vue'
+import InformationDiv from '@/components/SubmitReport/InformationDiv.vue'
 
 // Props
 const props = defineProps({
@@ -130,18 +125,4 @@ function handleSubmit() {
         font-weight: 900;
     }
 
-    .info-container {
-        margin-top: 1rem;
-        border: 3px solid rgba(152, 155, 0, 0.58);
-        background: rgba(255, 238, 0, 0.12);
-        padding: 1rem;
-        color: rgba(0, 0, 0, 0.648);
-
-        display: flex;
-        align-items: center;
-    }
-
-    .info-text {
-        margin-left: 1rem;
-    }
 </style>
---
+++ src/components/SubmitReport/InformationDiv.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="info-container" v-if="infoVisible">
+        <v-icon name="bi-book" scale="2" />
+        <div class="info-text">
+            <slot />
+        </div>      
+    </div>
+</template>
+<script setup>
+    const props = defineProps({
+        infoVisible: {
+            type: Boolean,
+            default: false
+        }
+    })
+</script>
+<style lang="scss" scoped>
+    .info-container {
+        margin-top: 1rem;
+        border: 3px solid rgba(152, 155, 0, 0.58);
+        background: rgba(255, 238, 0, 0.12);
+        padding: 1rem;
+        color: rgba(0, 0, 0, 0.648);
+
+        display: flex;
+        align-items: center;
+    }
+    .info-text {
+        margin-left: 1rem;
+    }
+</style>    
