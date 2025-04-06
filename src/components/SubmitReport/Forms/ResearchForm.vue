85bcea6ef287f5bb807e076597523317e55afb4f
Randall Alquicer <randall.bposeats@gmail.com>
2025-04-06 22:50:38 +0800

    clean up

--- src/components/SubmitReport/Forms/ResearchForm.vue
+++ src/components/SubmitReport/Forms/ResearchForm.vue
@@ -1,55 +1,56 @@
 <template>
-    <div class="form">
-      <div class="form-group">
-        <label>Title</label>
-        <input type="text" v-model="form.title" placeholder="Title" />
-        <span v-if="errors.title" class="error">{{ errors.title }}</span>
-      </div>
-  
-      <div class="form-group">
-        <label>Number of Months in Original Timeframe</label>
-        <input type="text" v-model="form.months" placeholder="Months" />
-        <span v-if="errors.months" class="error">{{ errors.months }}</span>
-      </div>
-  
-      <div class="form-group">
-        <label>Start Date</label>
-        <BaseDateInput v-model="form.startDate" width="100%" />
-        <span v-if="errors.startDate" class="error">{{ errors.startDate }}</span>
-      </div>
-  
-      <div class="form-group">
-        <label>End Date</label>
-        <BaseDateInput v-model="form.endDate" width="100%" />
-        <span v-if="errors.endDate" class="error">{{ errors.endDate }}</span>
-      </div>
-  
-      <div class="form-group">
-        <label>Names of Researcher/s</label>
-        <input type="text" v-model="form.researchers" placeholder="Names" />
-        <span v-if="errors.researchers" class="error">{{ errors.researchers }}</span>
-      </div>
-
-      <div class="form-group">
-        <label>Source of Majority Share of this Research Funding</label>
-        <BaseSelectInput v-model="form.sourceOfMajorityShare" :options="sourceOfMajorityShare" style="width: 100%; height: 38px;"/>
-      </div>
+  <div class="form">
+    <div class="form-group">
+      <label>Title</label>
+      <input type="text" v-model="form.title" placeholder="Title" />
+      <span v-if="errors.title" class="error">{{ errors.title }}</span>
     </div>
-  </template>
+
+    <div class="form-group">
+      <label>Number of Months in Original Timeframe</label>
+      <input type="text" v-model="form.months" placeholder="Months" />
+      <span v-if="errors.months" class="error">{{ errors.months }}</span>
+    </div>
+
+    <div class="form-group">
+      <label>Start Date</label>
+      <BaseDateInput v-model="form.startDate" width="100%" />
+      <span v-if="errors.startDate" class="error">{{ errors.startDate }}</span>
+    </div>
+
+    <div class="form-group">
+      <label>End Date</label>
+      <BaseDateInput v-model="form.endDate" width="100%" />
+      <span v-if="errors.endDate" class="error">{{ errors.endDate }}</span>
+    </div>
+
+    <div class="form-group">
+      <label>Names of Researcher/s</label>
+      <input type="text" v-model="form.researchers" placeholder="Names" />
+      <span v-if="errors.researchers" class="error">{{ errors.researchers }}</span>
+    </div>
+
+    <div class="form-group">
+      <label>Source of Majority Share of this Research Funding</label>
+      <BaseSelectInput v-model="form.sourceOfMajorityShare" :options="sourceOfMajorityShare" style="width: 100%; height: 38px;"/>
+    </div>
+  </div>
+</template>
   
-  <script setup>
+<script setup>
   import { reactive, ref } from 'vue'
   import BaseDateInput from '@/components/Global/BaseDateInput.vue'
   import BaseSelectInput from '@/components/Global/BaseSelectInput.vue'
 
-
-  //probably should be fetched from the database
+  // probably should be fetched from the database
   const sourceOfMajorityShare = ref([
     { value: 'up-entity', label: 'Local' },
     { value: 'rp-governemnt-entity-or-public-sector-entity', label: 'RP Government Entity or Public Sector Entity' },
     { value: 'rp-private-sector-entity', label: 'RP Private Sector Entity' },
     { value: 'foreign-or-nondomestic-entity', label: 'Foreign or Non-Domestic Entity' },
   ])
+
+  // reactives
   const form = reactive({
     title: '',
     months: '',
@@ -58,7 +59,6 @@
     researchers: '',
     sourceOfMajorityShare: '',
   })
-  
   const errors = reactive({
     title: '',
     months: '',
@@ -67,10 +67,10 @@
     researchers: '',
     sourceOfMajorityShare: '',
   })
-  
+
+  // functions
   function validateForm() {
     let valid = true
-  
     errors.title = form.title ? '' : 'Title is required.'
     errors.months = form.months && !isNaN(form.months) ? '' : 'Valid months are required.'
     errors.startDate = form.startDate ? '' : 'Start date is required.'
@@ -82,14 +82,13 @@
       errors.endDate = 'End date must be after start date.'
       valid = false
     }
-  
+
     for (const key in errors) {
       if (errors[key]) valid = false
     }
-  
+
     return valid
   }
-  
   function submitForm() {
     if (validateForm()) {
       // Replace this with your actual submit logic (e.g., API call)
@@ -100,17 +99,17 @@
       return null
     }
   }
-  
+
   // Expose the submitForm method to parent
   defineExpose({
     submitForm,
   })
-  </script>
-  
-  <style scoped>
+</script>
+
+<style scoped>
   .error {
     color: red;
     font-size: 0.8rem;
   }
-  </style>
+</style>
   
