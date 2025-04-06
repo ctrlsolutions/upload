4ac137fd014ce75e0b5a5016c890e1d1c1726f63
Randall Alquicer <randall.bposeats@gmail.com>
2025-04-06 19:46:39 +0800

    adds publicaitonform

--- src/components/SubmitReport/Forms/PublicationForm.vue
+++ src/components/SubmitReport/Forms/PublicationForm.vue
@@ -1,10 +1,127 @@
 <template>
-    <div class="form">fom</div>
+    <div class="form">
+      <div class="form-group">
+        <label>Title of Publication</label>
+        <input type="text" v-model="form.title" placeholder="Title" />
+      </div>
+  
+      <div class="form-group">
+        <label>Type of Publication</label>
+        <BaseSelectInput v-model="form.typeOfPublication" :options="typeOfPublication" />
+        <input type="text" v-model="form.typeOfPublicationOther" v-if="form.typeOfPublication === 'Other'"/>
+      </div>
+  
+      <div class="form-group">
+        <label>Name of Author/Co-Authors</label>
+        <input type="text" v-model="form.nameOfAuthor" />
+      </div>
+
+      <div class="form-group">
+        <label>Date Published or Accepted for Publication</label>
+        <BaseDateInput v-model="form.datePublished" width="100%" />
+      </div>
+  
+      <div class="form-group">
+        <label>Name of Journal/Book/Conference Publication/Other Publication</label>
+        <input type="text" v-model="form.nameOfPublication" />
+      </div>
+
+      <div class="form-group">
+        <label>Type of Publisher</label>
+        <base-select-input v-model="form.typeOfPublisher" :options="typeOfPublisher" />
+      </div>
+
+      <div class="form-group">
+        <label>Location of Publisher</label>
+        <BaseSelectInput v-model="form.locationOfPublisher" :options="[{value: 'local', label: 'Local'}, {value: 'international', label: 'International'}]" />
+      </div>
+
+      <div class="form-group">
+        <label>Volume No. and Issue No.</label>
+        <input type="text" v-model="form.volumeNo" />
+      </div>
+
+      <div class="form-group">
+        <label>DOI or URL</label>
+        <input type="text" v-model="form.doiOrUrl" />
+      </div>
+
+      <div class="form-group">
+        <label>Indicate other reputable collection/database</label>
+        <input type="text" v-model="form.otherReputableCollection" />
+      </div>
+
+      <div class="form-group">
+        <label>Number of Citations</label>
+        <input type="text" v-model="form.numberOfCitations" />
+      </div>
+
+      <div class="form-group">
+        <label>Supporting Evidences</label>
+        <br><h4>PDF Image File 1: Proof of Publication or Accepted for Publication such as cover, title and bibliographic information pages, publisher's acceptance letter or preprint, etc.</h4> 
+        <br><h4>PDF Image File 2: Proof of Utilization, if applicable, such as citation evidence, number of sales, UP International Publication Award, or was published in indexed/catalogued/recognized publications or by reputable publishers, etc., must be properly endorsed by the Dean/Head of Unit. If you have indicated a positive number of citations, please provide citation evidence for this publication.</h4>
+      </div>
+        
+    </div>
 </template>
 
-<style lang="scss" scoped>
-.form {
-    display: flex;
-    flex-direction: column;
-}
-</style>
+<script setup>
+import { reactive, ref } from 'vue'
+import BaseSelectInput from '@/components/Global/BaseSelectInput.vue'
+import BaseDateInput from '@/components/Global/BaseDateInput.vue'
+
+const form = reactive({
+    title: '',
+    typeOfPublication: '',
+    typeOfPublicationOther: '',
+    nameOfAuthor: '',
+    datePublished: null,
+    nameOfPublication: '',
+    typeOfPublisher: '',
+    locationOfPublisher: '',
+    volumeNo: '',
+    doiOrUrl: '',
+    otherReputableCollection: '',
+    numberOfCitations: '',
+})
+
+
+// probably pull this from the database
+const typeOfPublication = ref([
+    {
+        value: 'Peer-Reviewed Journal',
+        label: 'Peer-Reviewed Journal'
+    },
+    {
+        value: 'Book/Monograph',
+        label: 'Book/Monograph'
+    },
+    {
+        value: 'Edited/Peer-Reviewed Book Chapter',
+        label: 'Edited/Peer-Reviewed Book Chapter'
+    },
+    {
+        value: 'Peer-Reviewed Conference Paper Publication',
+        label: 'Peer-Reviewed Conference Paper Publication'
+    },
+    {
+        value: 'Other',
+        label: 'Other'
+    }
+])
+const typeOfPublisher = ref([
+    {
+        value: 'Commercial',
+        label: 'Commercial'
+    },
+    {
+        value: 'learned-society-and-association',
+        label: 'Learned Society and Association'
+    },
+    {
+        value: 'university-press',
+        label: 'University Press'
+    },
+
+])
+</script>
