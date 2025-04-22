955ce0611d226f1411ea0f8917b882d62cf09706
Az <arbusinos@up.edu.ph>
2025-04-22 17:55:13 +0800

    [refactor][az] reverts back to past routing

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,9 @@
 <template>
-  <component :is="layoutComponent">
-    <RouterView />
-  </component>
+  <RouterView />
 </template>
 
 <script setup lang="ts">
-import { RouterView, useRoute } from 'vue-router'
-
-import AuthLayout from '@/layouts/AuthPagesLayout.vue'
-import MainLayout from '@/layouts/AuthenticatedPagesLayout.vue'
-import LandingLayout from '@/layouts/LandingPageLayout.vue'
-import { computed } from 'vue'
-
-const route = useRoute()
-
-const layoutComponent = computed(() => {
-  switch (route.meta.layout) {
-    case 'auth':
-      return AuthLayout
-    case 'main':
-      return MainLayout
-    case 'landing':
-      return LandingLayout
-  }
-})
+import { RouterView } from 'vue-router'
 </script>
 
 <style scoped></style>
