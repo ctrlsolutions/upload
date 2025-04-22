8dae381faafe21b3cfb49c31b5ee3747d6a901d4
Az <arbusinos@up.edu.ph>
2025-04-22 13:45:39 +0800

    [refactor][az] adds layout switching

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
 <template>
-  <div class="container">
+  <component :is="layoutComponent">
     <RouterView />
-  </div>
+  </component>
 </template>
 
-<script setup>
-import { RouterView } from 'vue-router'
+<script setup lang="ts">
+import { RouterView, useRoute } from 'vue-router'
+
+import AuthLayout from '@/layouts/AuthPagesLayout.vue'
+import MainLayout from '@/layouts/AuthenticatedPagesLayout.vue'
+import LandingLayout from '@/layouts/LandingPageLayout.vue'
+import { computed } from 'vue'
+
+const route = useRoute()
+
+const layoutComponent = computed(() => {
+  switch (route.meta.layout) {
+    case 'auth':
+      return AuthLayout
+    case 'main':
+      return MainLayout
+    case 'landing':
+      return LandingLayout
+  }
+})
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
